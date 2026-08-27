8bfb0f45e79a92816ded897647094625eb2dc861
hugo <werwer748@naver.com>
2026-08-27 11:00:44 +0900

    재귀함수
    실행순서는 LIFO
    값을 올리려면 조금더 생각할 것

--- sec06/ex01/ex01.py
+++ sec06/ex01/ex01.py
@@ -6,7 +6,7 @@
 
 #? 재귀함수
 ? - 자기가 자기자신을 반복해서 호출하는 것.
-? - 반복문의 대체제
+? - 반복문의 대체제(알고리즘 측면에서...)
 ? - 다중 for문의 경우 코드의 유연성이 떨어지는데 재귀함수를 통해 해결할 수 있다.
 ? - 재귀함수는 스택을 활용하여 실행 된다.
 ? - 즉, 가장 늦게 호출된 함수가 가장 먼저 실행되는 것.
@@ -18,9 +18,40 @@
 """
 
 import sys
-sys.stdin = open("in1.txt")
+sys.stdin = open("in3.txt")
+
+# 내 풀이3 - 실행순서로 풀어보기
+n = int(input())
+def to_binary(x):
+    if x < 2:
+        print(x, end='')
+        return
+    else:
+        to_binary(x // 2)
+        print(x % 2, end='')
 
 
+to_binary(n)
+
+
+
+
+'''
+# 내 풀이2
+n = int(input())
+def to_binary(cur, prev):
+    if cur < 2:
+        return str(cur % 2) + prev
+    else:
+        prev = str(cur % 2) + prev
+        return to_binary(cur // 2, prev)
+
+
+print(to_binary(n, ''))
+'''
+
+
+'''
 # 강사 풀이
 def DFS(x):
     if x == 0:
@@ -33,7 +64,8 @@ def DFS(x):
 if __name__ == "__main__":
     n = int(input())
     # 깊이우선탐색의 줄임말 - DFS
-    DFS(n)
+    DFS(11)
+'''
 
 """
 # 내가 그냥 해본거
