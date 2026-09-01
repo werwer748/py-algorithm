b3e4d4c2584cabd5cab2e5f303f5ce0cf32257fd
hugo <werwer748@naver.com>
2026-09-01 15:46:18 +0900

    이진트리탐색
    전위, 중위, 후위 탐색

--- sec06/ex02/ex02.py
+++ sec06/ex02/ex02.py
@@ -15,35 +15,51 @@ ex) 4 5 2 6 7 3 1
 """
 
 #* 전위 순회 출력 - 자기의 일을 먼저 처리
-def DFS1(v):
+# def DFS1(v):
+#     if v > 7:
+#         return
+#     print(v, end=' ')
+#     DFS1(v * 2)
+#     DFS1(v * 2 + 1)
+# DFS1(1)
+
+def DFS1_2(v):
     if v > 7:
-        return
-    else:
-        print(v, end=' ')
-        DFS1(v * 2)
-        DFS1(v * 2 + 1)
+        return ""
+
+    return str(v) + " " + str(DFS1_2(v * 2)) + str(DFS1_2(v * 2 + 1))
+res_1 = DFS1_2(1).strip()
+print(res_1)
+
+
 
 #* 중위 순회 출력 - 자기의 일을 중간에 처리
-def DFS2(v):
+# def DFS2(v):
+#     if v > 7:
+#         return
+#     DFS2(v * 2)
+#     print(v, end=' ')
+#     DFS2(v * 2 + 1)
+# DFS2(1)
+def DFS2_2(v):
     if v > 7:
-        return
-    else:
-        DFS2(v * 2)
-        print(v, end=' ')
-        DFS2(v * 2 + 1)
+        return ""
+    return str(DFS2_2(v * 2)) + str(v) + " " + str(DFS2_2(v * 2 + 1))
+res_2 = DFS2_2(1)
+print(res_2)
+
 
 #* 후위 순회 출력 - 자기의 일을 마지막에 처리
-def DFS3(v):
+# def DFS3(v):
+#     if v > 7:
+#         return
+#     DFS3(v * 2)
+#     DFS3(v * 2 + 1)
+#     print(v, end=' ')
+# DFS3(1)
+def DFS3_2(v):
     if v > 7:
-        return
-    else:
-        DFS3(v * 2)
-        DFS3(v * 2 + 1)
-        print(v, end=' ')
-
-if __name__ == "__main__":
-    DFS1(1)
-    print('', end="\n")
-    DFS2(1)
-    print('', end="\n")
-    DFS3(1)
+        return ""
+    return str(DFS3_2(v * 2)) + str(DFS3_2(v * 2 + 1)) + str(v) + " "
+res_3 = DFS3_2(1)
+print(res_3)
